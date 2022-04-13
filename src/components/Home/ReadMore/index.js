b145d97118e3a5b35ba9550a5e933290a095b680
TehranMammadov01 <tehranmammadov.dev.01@gmail.com>
2022-04-13 19:36:23 +0400

    read more page data change to json

--- src/components/Home/ReadMore/index.js
+++ src/components/Home/ReadMore/index.js
@@ -1,41 +1,26 @@
+import { useState } from 'react';
 import './ReadMore.scss';
-import girlSitOnChair from '../../../assets/images/sit-on-chair.png';
-import readMoreImg from '../../../assets/images/read-more.png';
+import readMoreData from './ReadMore.json';
 
 const ReadMore = () => {
+    const [moreData, setMoreData] = useState(readMoreData);
+    
     return (
         <section className="read-more">
-            <div className="read-more__container-one">
-                <div className="read-more__container-one__image">
-                    <img src={girlSitOnChair} alt="" />
-                </div>
-                <div className="read-more__container-one__description">
-                    <p>
-                        Ultrices in phasellus suspendisse et, urna. Fermentum elementum aliquam mattis luctus. 
-                        Eu orci aliquam tincidunt enim. Facilisis odio eget morbi at nulla nisi. Mauris, ipsum convallis diam gravida tristique...
-                    </p>
-                    <div className="read-more-btn">
-                        <button>Read more</button>
+            {moreData.map((data, index) => (
+                <div key={index} className="read-more__container">
+                    <div className="read-more__container__image">
+                        <img src={data.src} alt="" />
                     </div>
-                    <h2>Sed cras ut vulputate a risus</h2>
-                </div>
-                {/* <div className="linne"></div> */}
-            </div>
-            <div className="read-more__container-two">
-                <div className="read-more__container-two__description">
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. <span>Maecenas tortor</span> id 
-                        ut a tempor. In aliquam amet est mauris velit, venenatis tempor proin...
-                    </p>
-                    <div className="read-more-btn">
-                        <button>Read more</button>
+                    <div className="read-more__container__description">
+                        <p><span>{data.bottomLineDesc}</span> {data.description}</p>
+                        <div className="read-more-btn">
+                            <button>Read more</button>
+                        </div>
+                        <h2>{data.title}</h2>
                     </div>
-                    <h2>Purus eget molestie urna</h2>
                 </div>
-                <div className="read-more__container-two__image">
-                    <img src={readMoreImg} alt="" />
-                </div>
-            </div>
+            ))}
         </section>
     )
 }
