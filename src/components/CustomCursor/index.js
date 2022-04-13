96b2ef75ef258d6dee30693ebd6a5eb44437956d
azadsarxanli <azad.sarxanli@gmail.com>
2022-04-13 11:59:59 +0400

    cursor and header

--- src/components/CustomCursor/index.js
+++ src/components/CustomCursor/index.js
@@ -1,23 +1,23 @@
-import { useEffect, useRef } from 'react';
-import './CustomCursor.scss';
+import { useEffect, useRef } from "react";
+import "./CustomCursor.scss";
 
 const CustomCursor = () => {
-    const appCursor = useRef(null);
+  const appCursor = useRef(null);
 
-    useEffect(() => {
-        document.addEventListener("mousemove", (event) => {
-            const { clientX, clientY } = event;
-            const mouseX = clientX - appCursor.current.clientWidth / 2;
-            const mouseY = clientY - appCursor.current.clientHeight / 2;
-            appCursor.current.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
-        });
-    
-        return () => {};
-    }, []);
+  useEffect(() => {
+    document.addEventListener("mousemove", (event) => {
+      //   const { clientX, clientY } = event;
+      //   const mouseX = clientX - appCursor.current.clientWidth / 2;
+      //   const mouseY = clientY - appCursor.current.clientHeight / 2;
+      //   appCursor.current.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+      appCursor.current.style.top = event.clientY + "px";
+      appCursor.current.style.left = event.clientX + "px";
+    });
 
-    return (
-        <div className="app-cursor " ref={appCursor}></div>
-    )
-}
+    return () => {};
+  }, [appCursor]);
+
+  return <div className="app-cursor " ref={appCursor}></div>;
+};
 
 export default CustomCursor;
