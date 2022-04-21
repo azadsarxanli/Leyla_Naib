60bdd49b12ee0992e640218482c96fbfcb2c77f9
azadsarxanli <azad.sarxanli@gmail.com>
2022-04-21 14:26:25 +0400

    app js

--- src/App.js
+++ src/App.js
@@ -27,7 +27,6 @@ const App = () => {
         <Route path="/" element={<Home />}></Route>
         <Route path="about" element={<About />}></Route>
       </Routes>
-
       <Footer />
     </>
   );
