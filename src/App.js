e732be6a61d705b2cb1c337c1191b641bace797e
azadsarxanli <azad.sarxanli@gmail.com>
2022-04-25 11:55:59 +0400

    git commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 import CustomCursor from "./components/CustomCursor";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Home from "./components/Home/";
+import Home from "./components/Home/Home";
 import "./styles/styles.scss";
-import About from "./components/About/About";
+import About from "./components/About";
 import NotFound from "./components/NotFound";
 
 const App = () => {
@@ -20,7 +20,6 @@ const App = () => {
   const [windowScrollY, setWindowScrollY] = useState(0);
   document.onscroll = () => {
     setWindowScrollY(window.scrollY);
-    console.log("helli");
   };
 
   return (
@@ -32,13 +31,13 @@ const App = () => {
       />
       <CustomCursor cursor={cursor} />
       <Routes>
-        {/* // Use it in this way, and it should work: */}
-        <Route path="*" element={<NotFound />} />
         <Route
           path="/"
           element={<Home windowScrollY={windowScrollY} />}
         ></Route>
         <Route path="about" element={<About />}></Route>
+        {/* // for not found page*/}
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
     </>
---
+++ src/components/About/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./About.scss";
+
+const About = () => {
+  return <section>hay guys</section>;
+};
+
+export default About;
