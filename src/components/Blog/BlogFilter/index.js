de6fa798b9ed654eb7d43e0dd5cf35c0c2508fdd
azadsarxanli <azad.sarxanli@gmail.com>
2022-05-16 23:26:29 +0400

    blog and work filter contact changes

--- src/components/Blog/BlogFilter/index.js
+++ src/components/Blog/BlogFilter/index.js
@@ -5,11 +5,14 @@ import { useState } from "react";
 import "./BlogFilter.scss";
 
 const BlogFilter = ({ setCategoryName, setVal, blogData }) => {
+  const [inputRangeValue, setInputRangeValue] = useState(10);
+
   const subHeaderRef = useRef();
   const [length, setLength] = useState(0); //? @Dev dynamically maximum value for input range
 
   const rangeChangeHandler = (event) => {
     const subHeader = subHeaderRef.current;
+    setInputRangeValue(event.target.value);
     const paragraphs = [...subHeader.querySelectorAll("p")];
     paragraphs.forEach((item, index) => {
       if (item.className.includes(event.target.value / 10)) {
@@ -27,17 +30,33 @@ const BlogFilter = ({ setCategoryName, setVal, blogData }) => {
 
   const allCategoriesName = ["ALL"];
   if (blogData) {
-    blogData.map(data => {
+    blogData.map((data) => {
       allCategoriesName.push(data.category);
-    })
+    });
   }
   let uniqueNames = [...new Set(allCategoriesName)];
+  const onClickHandler = (event, index) => {
+    setCategoryName(event.target.textContent.toLowerCase());
+    if (index === 0) {
+      setVal(1);
+      setInputRangeValue(10);
+    } else {
+      setVal(index);
+      setInputRangeValue((index + 1) * 10 - 5);
+    }
+  };
 
   return (
     <section className="blog-filter">
       <div ref={subHeaderRef} className="blog-filter__subheader">
         {uniqueNames?.map((categoryName, index) => (
-          <p key={index} className={`paragraphs paragraph-${index + 1}`}>{categoryName}</p>
+          <p
+            onClick={(event) => onClickHandler(event, index)}
+            key={index}
+            className={`paragraphs paragraph-${index + 1}`}
+          >
+            {categoryName}
+          </p>
         ))}
       </div>
       <div className="blog-filter__range">
@@ -45,6 +64,7 @@ const BlogFilter = ({ setCategoryName, setVal, blogData }) => {
           onChange={rangeChangeHandler}
           min={10}
           max={length}
+          value={inputRangeValue}
           type="range"
         />
       </div>
--- src/components/Work/WorkFilter/index.js
+++ src/components/Work/WorkFilter/index.js
@@ -4,11 +4,16 @@ import { useRef } from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 
-const WorkFilter = ({ setCategoryName, setVal, workData }) => {
+const WorkFilter = ({ setCategoryName, setVal, workData, categoryName }) => {
+  const [inputRangeValue, setInputRangeValue] = useState(10);
   const subHeaderRef = useRef();
+  const inputRef = useRef();
+
   const [length, setLength] = useState(0); //? @Dev dynamically maximum value for input range
 
   const rangeChangeHandler = (event) => {
+    setInputRangeValue(event.target.value);
+
     const subHeader = subHeaderRef.current;
     const paragraphs = [...subHeader.querySelectorAll("p")];
     paragraphs.forEach((item, index) => {
@@ -33,22 +38,37 @@ const WorkFilter = ({ setCategoryName, setVal, workData }) => {
     });
   }
   let uniqueNames = [...new Set(allCategoriesName)];
-  console.log(allCategoriesName);
+  const onClickHandler = (event, index) => {
+    setCategoryName(event.target.textContent.toLowerCase());
+    if (index === 0) {
+      setVal(1);
+      setInputRangeValue(10);
+    } else {
+      setVal(index);
+      setInputRangeValue((index + 1) * 10);
+    }
+  };
 
   return (
     <section className="work-filter">
       <div className="work-filter__subheader" ref={subHeaderRef}>
         {uniqueNames?.map((categoryName, index) => (
-          <p key={index} className={`paragraphs paragraph-${index + 1}`}>
+          <p
+            onClick={(event) => onClickHandler(event, index)}
+            key={index}
+            className={`paragraphs paragraph-${index + 1}`}
+          >
             {categoryName}
           </p>
         ))}
       </div>
       <div className="work-filter__range">
         <input
+          ref={inputRef}
           onChange={rangeChangeHandler}
           min={10}
           max={length}
+          value={inputRangeValue}
           type="range"
         />
       </div>
